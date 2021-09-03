321c577e91f97ecdf2c1c83eb2215a790fd13221
landonbadgett <badgettlj@g.cofc.edu>
2021-09-03 11:39:01 -0400

    completed hw1

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,2 +1,14 @@
+"""
+Name: Landon Badgett
+hello.py
+
+Problem: Print hello world
+
+Certification of Authenticity:
+I certify that this assignment is entirely my own work.
+"""
+
 def main():
-    print("hello,world")
+    print("hello, world!")
+
+main()
